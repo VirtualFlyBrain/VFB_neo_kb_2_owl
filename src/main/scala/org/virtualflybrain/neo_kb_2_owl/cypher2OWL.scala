dc4fc6081b4d40fcc8e78384c5a885d3f92136c6
dosumis <dosumis@gmail.com>
2018-05-09 23:12:37 +0100

    Reducing verbosity cypher2OWL; adding print statements for tracking progress to def roller.

--- src/main/scala/org/virtualflybrain/neo_kb_2_owl/cypher2OWL.scala
+++ src/main/scala/org/virtualflybrain/neo_kb_2_owl/cypher2OWL.scala
@@ -62,7 +62,7 @@ class cypher2OWL(bs: BrainScowl, session: Session, dataset: String) {
       // TODO: call to function to check if i already in owl, add label if not
       val c = Class(record.get("c.iri").asString)
       if (record.get("rel_typ").asString == "INSTANCEOF") {
-        print(s"""Adding $i Type $c \n""")
+        //print(s"""Adding $i Type $c \n""")
         this.bs.add_axiom(i Type c)
       } else if (record.get("rel_typ").asString == "Related") {
         val r = ObjectProperty(record.get("r.iri").asString)
@@ -141,11 +141,11 @@ class cypher2OWL(bs: BrainScowl, session: Session, dataset: String) {
         // Adding typing to J: This needs some refactoring to pull out generic typing into new method.
         val c = Class(record.get("c.iri").asString) 
         if (record.get("rel_typ").asString == "INSTANCEOF") {
-          print(s"""Adding $i Type $c \n""")
+          //print(s"""Adding $i Type $c \n""")
           this.bs.add_axiom(i Type c)
         } else if (record.get("rel_typ").asString == "Related") {
           val r = ObjectProperty(record.get("r.iri").asString)
-          print(s"""Adding $i Type $r some $c \n""")
+          //print(s"""Adding $i Type $r some $c \n""")
           this.bs.add_axiom(i Type (r some c))
         } else {
           /// Add in a warning or fail.
